9327161c35630c929fe9e78b4d391d69bd1b76eb
junjios <juri.joussen@gmail.com>
2025-08-08 01:19:41 +0200

    build essentia with FFTW instead of KISS for Fourier transforms rearrange Dockerfile for more flexibility add extensive comments

--- android/essentia_build/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker build -t essentia-android .
-
-docker create --name temp-container essentia-android sleep 10
-
-docker cp temp-container:/build/. ../app/src/main/jniLibs
-
-docker rm temp-container
