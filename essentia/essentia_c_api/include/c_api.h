a44ae25ba04947f29b6ba35379bb135d528f88d0
visualizetheory <visualizetheory@gmail.com>
2025-09-04 22:28:06 +0200

    Removed unused functions

--- essentia/essentia_c_api/include/c_api.h
+++ essentia/essentia_c_api/include/c_api.h
@@ -12,7 +12,7 @@ void essentia_shutdown();
 typedef struct {
 	char* key;            // C string (must be freed)
 	float duration;       // Duration in seconds
-	float* chromagram;    // Flat array, size: n_frames * n_bins
+	float* chromagram;    // Flat array, size: n_frames * n_bins (must be freed)
 	int chroma_n_frames;  // Number of frames (rows)
 	int chroma_n_bins;    // Number of bins (columns)
 } EssentiaAnalysisResult;
@@ -22,15 +22,6 @@ EssentiaAnalysisResult* essentia_analyze_buffer(const float* buffer, int buffer_
 
 // Free an EssentiaAnalysisResult and all its heap-allocated fields
 void delete_analysis_result(EssentiaAnalysisResult* result);
-
-// Computes the key from a raw float32 audio file. Returns a newly allocated C string (must be freed with delete_c_string)
-const char* compute_key_from_file(const char* file_path);
-
-// Computes the key from a float* buffer and its length. Returns a newly allocated C string (must be freed with delete_c_string)
-const char* compute_key_from_float_buffer(const float* buffer, int length);
-
-// Frees a C string allocated by compute_key_from_file
-void delete_c_string(char* str);
 
 #ifdef __cplusplus
 }
