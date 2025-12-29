1321f05b2da0f64b0eed80922a6a8c6ec69483a7
visualizetheory <visualizetheory@gmail.com>
2025-12-29 21:34:02 +0100

    Added fling motion for scrolling

--- lib/screens/visualize.dart
+++ lib/screens/visualize.dart
@@ -29,9 +29,16 @@ class Visualizer extends StatefulWidget {
 class _VisualizerState extends State<Visualizer> with SingleTickerProviderStateMixin {
   
   double currentTime = 0.0; // Current time in seconds. Visuals are based on this variable.
-  double initialTime = 0.0; // Auxilliary variable to track time when pausing/playing
+  late double initialTime; // Auxilliary variable to track time when pausing/playing
 
-  late final Ticker _ticker; // Ticker for updating current time
+  bool isPlaying = false; // Track whether audio/visuals are playing
+  late bool resumePlayingAfterFling; // Auxilliary variable to track if playback should resume after scrolling
+
+  late final Ticker _timeTicker; // Ticker for updating current time
+
+  late Ticker _flingTicker; // Ticker for fling animation
+  late double _flingVelocity; // Velocity of fling in seconds per second
+  bool isFlinging = false; // Track whether a fling animation is in progress
 
   final ap.AudioPlayer _player = ap.AudioPlayer()..setReleaseMode(ap.ReleaseMode.stop); // Audio player
 
@@ -39,17 +46,10 @@ class _VisualizerState extends State<Visualizer> with SingleTickerProviderStateM
   @override
   void initState() {
     super.initState();
-    // Initialize ticker
-    _ticker = Ticker((elapsed) {
-      if (!mounted) return; // Guard against unmounted state
-      setState(() {
-        currentTime = initialTime + elapsed.inMilliseconds / 1000.0; // Updtate current time
-        if (currentTime >= widget.duration) { // Pause visuals and audio if end is reached
-          currentTime = 0.0;
-          pause();
-        }
-      });
-    });
+
+    // Initialize tickers
+    _timeTicker = Ticker(_timeUpdate);
+    _flingTicker = Ticker(_flingUpdate);
 
     // Set up audio player
     _player.onPlayerStateChanged.listen((state) { // Listen to state changes
@@ -59,7 +59,7 @@ class _VisualizerState extends State<Visualizer> with SingleTickerProviderStateM
     _player.onPlayerComplete.listen((_) { // Listen to completion event
       if (!mounted) return;
       setState(() {});
-      _ticker.stop();
+      _timeTicker.stop();
     });
     _player.setSource(_resolveSource(widget.audioUrl)); // Set audio source
   }
@@ -67,29 +67,42 @@ class _VisualizerState extends State<Visualizer> with SingleTickerProviderStateM
   // Clean up resources
   @override
   void dispose() {   
-    _ticker.dispose();
+    _timeTicker.dispose();
+    _flingTicker.dispose();
     _player.dispose();
     super.dispose();
   }
 
   // Play audio and start visualization
   void play() async {
-    if (!mounted) return;
-
-    initialTime = currentTime; // Update initial time 
+    initialTime = currentTime; // Fix initial time for ticker updates
 
     // Position audio to initial time and start playback
     await _player.seek(Duration(milliseconds: (initialTime * 1000).toInt()));
     await _player.resume();
-
-    // Start ticker to update current time
-    _ticker.start();
+    
+    _timeTicker.start(); // Start dynamic visuals by starting ticker
+    
+    isPlaying = true; // Update playing state
   }
 
   // Pause audio and visualization
   void pause() {
-    _ticker.stop(); // Stop ticker (elapsed time is reset to zero). 
+    _timeTicker.stop(); // Stop ticker (elapsed time is reset to zero). 
     _player.pause(); // Pause audio playback
+    isPlaying = false;
+  }
+
+  // Update current time based on elapsed duration. To be called by time ticker
+  void _timeUpdate(Duration elapsed) {
+      if (!mounted) return; // Guard against unmounted state
+      setState(() {
+        currentTime = initialTime + elapsed.inMilliseconds / 1000.0; // Updtate current time
+        if (currentTime >= widget.duration) { // Pause visuals and audio if end is reached
+          currentTime = widget.duration;
+          pause();
+        }
+      });
   }
 
   // Resolve audio source based on path type (asset, URL, or device file)
@@ -99,6 +112,57 @@ class _VisualizerState extends State<Visualizer> with SingleTickerProviderStateM
     } else {
       return kIsWeb ? ap.UrlSource(path) : ap.DeviceFileSource(path);
     }
+  }
+
+  // Start fling animation
+  void _startFling() {
+    initialTime = currentTime; // Fix initial time for fling ticker updates
+    _flingTicker.start(); // Start fling by starting ticker
+    isFlinging = true;
+  }
+
+  // Abort fling animation
+  void _abortFling() {
+    _flingTicker.stop(); // Stop fling by stopping ticker
+    isFlinging = false;
+  }
+
+  // End fling animation and resume playback if needed
+  void _endFling() {
+    _abortFling();
+    if (resumePlayingAfterFling) {
+      play();
+    }
+  }
+
+  // Update current time based on fling velocity. To be called by fling ticker
+  void _flingUpdate(Duration elapsed) {
+    if (!isFlinging) return;
+
+    // Fling motion is modelled by integrating a decaying exponential velocity function with initial velocity _flingVelocity
+    const double dampingFactor = 4.0;
+    double timeDelta = (_flingVelocity / dampingFactor) * (1 - exp(-dampingFactor * elapsed.inMilliseconds / 1000.0));
+    double timeDeltaLimit = (_flingVelocity / dampingFactor); 
+    double timeDiff = (timeDeltaLimit - timeDelta).abs();
+
+    if (!mounted) return;
+    setState(() {
+      currentTime = initialTime + timeDelta; 
+      if (currentTime < 0.0) {
+        currentTime = 0.0;
+        _endFling();
+        return;
+      } 
+      if (currentTime > widget.duration) {
+        currentTime = widget.duration;
+        _endFling();
+        return;
+      } 
+      if (timeDiff < 0.1) {
+        _endFling();
+        return;
+      } 
+    }); 
   }
 
   // Build the visualization UI
@@ -121,8 +185,6 @@ class _VisualizerState extends State<Visualizer> with SingleTickerProviderStateM
           double currentLinePx = availableHeight * (1/3); // Distance of current line from bottom 
           double pitchLinePx = currentLinePx - oneSecondPx; // Distance of pitch line from bottom 
           double deltaWidthPx = availableWidth / 15; // Horizontal offset for vertical lines (x coord difference)
-          
-          final isPlaying = (_player.state == ap.PlayerState.playing); // Check if audio is playing
 
           List<Widget> baseWidgets = [];
           Widget playButton = PlayButton(
@@ -246,18 +308,32 @@ class _VisualizerState extends State<Visualizer> with SingleTickerProviderStateM
           return GestureDetector(
             behavior: HitTestBehavior.opaque,
             onVerticalDragStart: (_) {
-              pause(); // Pause audio and ticker
+              if (isFlinging) {
+                _abortFling();
+                return;
+              }
+              
+              // Below, we want to resume playing if and only if the audio was playing before
+              if (isPlaying) { 
+                resumePlayingAfterFling = true;
+                pause();
+                return;
+              } 
+
+              resumePlayingAfterFling = false;
             },
             onVerticalDragUpdate: (details) {
+              if (!mounted) return;
               setState(() {
                 // Adjust currentTime: finger moving down -> positive delta -> increase time
                 currentTime += details.primaryDelta! / oneSecondPx;
                 currentTime = currentTime.clamp(0.0, widget.duration);
               });
             },
-            onVerticalDragEnd: (_) {
-              // Optionally: resume playback here if you want
-              // play();
+            onVerticalDragEnd: (details) {
+              // Fling effect
+              _flingVelocity = (details.primaryVelocity ?? 0.0) / oneSecondPx;
+              _startFling();
             },
             child: SizedBox.expand(
               child: Stack(
