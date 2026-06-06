f5edd63f2ebefea9c6b2a4e5a4e62d6d8b79a062
visualizetheory <visualizetheory@gmail.com>
2026-06-06 16:34:30 +0200

    Added a flag for specifying platform. This is necessary to make the Docker file work on mac. Also I moved the third_party directory inside 'audioanalysis' so it need not be mentioned in the Docker file anymore.

--- build_tools/docker/android/build_audioanalysis.sh
+++ build_tools/docker/android/build_audioanalysis.sh
@@ -2,6 +2,8 @@
 # Don't run this script directly. This intended to be run in a Docker container.
 # Build native C/C++ code for different Android ABIs using cmake.
 
+set -euo pipefail
+
 # Delete old make files and caches to guarantee a clean build if CLEAN_BUILD is set to true.
 if $CLEAN_BUILD; then
   echo "> Clean build directory..."
@@ -17,24 +19,24 @@ for ABI in $ANDROID_ABIS; do
   echo "> Building for Android ABI $ABI..."
 
   echo "  > Configure cmake..."
-    cmake -S "$SOURCE_DIR" \
-          -B "$BUILD_DIR/$ABI" \
-          -DPLATFORM=android \
-          -DANDROID_ABI="$ABI" \
-          -DANDROID_STL=c++_static \
-          -DANDROID_PLATFORM="$ANDROID_API_LEVEL" \
-          -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake" \
-          -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
-          2>&1 | sed 's/^/      /'
+  cmake -S "$SOURCE_DIR" \
+        -B "$BUILD_DIR/$ABI" \
+        -DPLATFORM=android \
+        -DANDROID_ABI="$ABI" \
+        -DANDROID_STL=c++_static \
+        -DANDROID_PLATFORM="$ANDROID_API_LEVEL" \
+        -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake" \
+        -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
+        2>&1 | sed 's/^/      /'
   echo "    Done."
 
   echo "  > Build shared libraries..."
-    cmake --build "$BUILD_DIR/$ABI" 2>&1 | sed 's/^/      /'
+  cmake --build "$BUILD_DIR/$ABI" 2>&1 | sed 's/^/      /'
   echo "    Done."
 
   echo "  > Copy built libraries to jniLibs..."
-    mkdir -p "$OUTPUT_DIR/$ABI"
-    cp "$BUILD_DIR/$ABI/libaudioanalysis.so" "$OUTPUT_DIR/$ABI/"
+  mkdir -p "$OUTPUT_DIR/$ABI"
+  cp "$BUILD_DIR/$ABI/libaudioanalysis.so" "$OUTPUT_DIR/$ABI/"
   echo "    Done."
 
   echo "  Build for ABI $ABI successful."
